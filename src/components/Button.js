9e2fb253dd4d6fc3ae6b00f89cd5aa76733668f0
NN125 <nicolas.nolden@student.swic.edu>
2025-12-01 18:46:50 -0600

    modify Button to accommodate this project

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,9 @@
-export default function Button({ disabled, type = "button", text }) {
+export default function Button({ type = "button", color = "bg-white" }) {
   return (
     <button
       type={type}
       disabled={disabled}
-      className="rounded bg-blue-600 px-4 py-2 text-white hover:bg-blue-700 disabled:cursor-not-allowed disabled:opacity-50"
-    >
-      {text}
-    </button>
+      className={`btnField${color}`}
+    ></button>
   );
 }
