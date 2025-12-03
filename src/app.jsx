5462d13cc1e9973de53da1d2e51a4a26063ab69e
NN125 <nicolas.nolden@student.swic.edu>
2025-12-03 15:42:15 -0600

    refactor first button to use Button import

--- src/app.jsx
+++ src/app.jsx
@@ -13,12 +13,11 @@ export default function app() {
         <div>
           {/* First row of buttons */}
           <div>
-            <button
+            <Button
               onClick={() => {
                 handleButtonClick("button1");
               }}
-              className="btnField"
-            ></button>
+            />
             <button
               onClick={() => {
                 handleButtonClick("button2");
