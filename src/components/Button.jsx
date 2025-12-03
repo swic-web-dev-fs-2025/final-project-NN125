653ad45ee0e8e666dd982f8772f9fc58f97df48c
NN125 <nicolas.nolden@student.swic.edu>
2025-12-03 15:45:02 -0600

    remove unneeded id parameter

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,10 @@
 export default function Button({
-  id,
   onClick,
   type = "button",
   color = "bg-white",
 }) {
   return (
     <button
-      id={id}
       type={type}
       className={`btnField ${color}`}
       onClick={onClick}
