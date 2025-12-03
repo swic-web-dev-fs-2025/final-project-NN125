02e123f419f4a31bee4935993af24b5bc25d8e7c
NN125 <nicolas.nolden@student.swic.edu>
2025-12-03 15:10:43 -0600

    remove old state management code form app

--- src/app.jsx
+++ src/app.jsx
@@ -1,31 +1,4 @@
-import { useLocalStorage } from "@uidotdev/usehooks";
-
 export default function app() {
-  const [buttonStates, setButtonStates] = useLocalStorage("buttonStates", {
-    button1: "bg-white",
-    button2: "bg-white",
-    button3: "bg-white",
-    button4: "bg-white",
-    button5: "bg-white",
-    button6: "bg-white",
-    button7: "bg-white",
-    button8: "bg-white",
-    button9: "bg-white",
-    button10: "bg-white",
-    button11: "bg-white",
-    button12: "bg-white",
-    button13: "bg-white",
-    button14: "bg-white",
-    button15: "bg-white",
-    button16: "bg-white",
-  });
-
-  const handleButtonClick = (buttonName) => {
-    setButtonStates((prevState) => ({
-      ...prevState,
-      [buttonName]: !prevState[buttonName],
-    }));
-  };
   return (
     <>
       <main className="min-h-screen bg-blue-900 flex flex-col items-center justify-center p-2">
