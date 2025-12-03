52561d7a7d9425ee0f78aa9801157c89d1e08cdd
NN125 <nicolas.nolden@student.swic.edu>
2025-12-03 15:42:29 -0600

    add id parameter to Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,12 @@
 export default function Button({
+  id,
   onClick,
   type = "button",
   color = "bg-white",
 }) {
   return (
     <button
+      id={id}
       type={type}
       className={`btnField ${color}`}
       onClick={onClick}
