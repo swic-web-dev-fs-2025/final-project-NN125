9fa667d711ea898da85183e989fe60618d171ce6
NN125 <nicolas.nolden@student.swic.edu>
2025-12-04 16:59:39 -0600

    set the savedData to use the same things inputted my the user

--- src/hooks/use-button-state.jsx
+++ src/hooks/use-button-state.jsx
@@ -41,6 +41,8 @@ export default function useButtonState() {
     const data = Object.fromEntries(formData);
     setSubmittedData(data);
     setMessage("");
+
+    setSavedData(data);
   }
 
   return {
