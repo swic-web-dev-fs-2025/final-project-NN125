ecccf342620990aca017eb3adcd625e7f8e0d40d
NN125 <nicolas.nolden@student.swic.edu>
2025-12-04 16:18:50 -0600

    add disabled parameter to Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,20 @@
 export default function Button({
   onClick,
   id,
+  disabled,
   type = "button",
   color = "bg-white",
   className = `btnField ${color}`,
   text,
 }) {
   return (
-    <button id={id} type={type} className={className} onClick={onClick}>
+    <button
+      id={id}
+      type={type}
+      className={className}
+      onClick={onClick}
+      disabled={disabled}
+    >
       {text}
     </button>
   );
